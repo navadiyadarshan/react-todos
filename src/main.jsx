80450e3ef93ae1d47a48c1a017d2b6a7d29ab833
Darshan <navadiyadarshan2666@gmail.com>
2025-05-02 22:31:07 +0530

    pwa update

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './tailwind.css'
 
 import App from './App.jsx'
-import Drag from './dragAndDrop.jsx'
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,39 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate', // Automatically updates the service worker when a new version is deployed
+      includeAssets: ['favicon.ico', 'robots.txt', 'apple-touch-icon.png'], // Include your assets here
+      manifest: {
+        name: 'To dos',
+        short_name: 'todos',
+        description: 'This is a to-do list app.',
+        start_url: '/',
+        display: 'standalone', // Fullscreen experience without browser UI
+        background_color: '#2A2A2A',
+        theme_color: '#2A2A2A',
+        icons: [
+          {
+            src: 'favicon.ico',
+            sizes: '192x192',
+            type: 'image/x-icon'
+          },
+          {
+            src: 'favicon.ico',
+            sizes: '512x512',
+            type: 'image/x-icon'
+          }
+        ]
+      },
+      workbox: {
+        // Workbox configuration to handle caching strategies
+        globPatterns: ['**/*.{js,css,html,png,jpg,jpeg,svg}'],
+      },
+    })
+  ]
 })
